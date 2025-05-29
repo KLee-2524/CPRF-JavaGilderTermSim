b8a30719ea3291e5aca7dd9e2fb0e0a5b72aba0f
KLee-2524 <klee.337x@gmail.com>
2025-05-28 23:04:45 -0700

    Match package name with repo name

--- JavaGilderTermSim.java
+++ JavaGilderTermSim.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-//package javagildertermsim;
+package CPRF_JavaGilderTermSim;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
